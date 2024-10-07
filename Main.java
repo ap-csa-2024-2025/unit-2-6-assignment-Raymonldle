8110ea33f15d90e66e0b50467b5560d7ddf1f8f6
RaymonIdle <variluoxx@gmail.com>
2024-10-07 11:17:42 -0700

    finished the hw

--- Main.java
+++ Main.java
@@ -4,6 +4,25 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // problem 1
+    
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Enter the radius of the circle: ");
+    double radius = userInput.nextDouble();
+    Circle circle = new Circle(radius);
+    System.out.println("A circle with a radius of " + circle.getRadius() + " has a circumference of " + circle.getCircumference() + " and an area of " + circle.getArea());
+
+    //problem 2
+    System.out.println("Enter number of sides: ");
+    int numSides = userInput.nextInt();
+
+    System.out.println("Enter the side length: ");
+    double sideLength = userInput.nextDouble();
+
+    RegularPolygon myPolygon = new RegularPolygon(numSides, sideLength);
+    System.out.println("Area with " + myPolygon.getNumSides() + " sides: " + myPolygon.getArea());
+    System.out.println("Incrementing the number of sides by two");
+    myPolygon.setNumSides(numSides + 2);
+    System.out.println("Area with " + myPolygon.getNumSides() + " sides: " + myPolygon.getArea());
   }
 }
